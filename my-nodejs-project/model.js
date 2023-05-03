c0d4bef265c968c7d1c6f6637e9cb2e1f098561d
Isaac Orsaah <66434358+isaacorsaah@users.noreply.github.com>
2023-05-03 17:53:28 +0100

    A user model file in my  Node.js project, which includes fields like email, password, role (admin/employee)

---
+++ my-nodejs-project/model.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  role: {
+    type: String,
+    enum: ['admin', 'employee'],
+    default: 'employee'
+  }
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
