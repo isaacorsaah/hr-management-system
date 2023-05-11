e9be915b3eeb60a787bcbcc59e0acc880b21316c
Isaac Orsaah <66434358+isaacorsaah@users.noreply.github.com>
2023-05-11 17:18:53 +0100

    Middleware function to verify JWT token

--- my-nodejs-project/utils/auth.js
+++ my-nodejs-project/utils/auth.js
@@ -1,41 +1,30 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+const secretKey = 'your-secret-key'; // Replace with your own secret key
 
-const app = express();
-
-// Set up body parser middleware
-app.use(bodyParser.json());
-
-// Connect to MongoDB
-mongoose.connect('mongodb://localhost/myapp', { useNewUrlParser: true })
-  .then(() => console.log('Connected to MongoDB'))
-  .catch(err => console.error('Error connecting to MongoDB', err));
-
-// Define user schema and model
-const userSchema = new mongoose.Schema({
-  email: String,
-  password: String,
-  role: String
-});
-
-const User = mongoose.model('User', userSchema);
-
-// Define login route
-app.post('/api/login', async (req, res) => {
-  const { email, password } = req.body;
-
-  // Look up user in database
-  const user = await User.findOne({ email, password });
-
-  if (!user) {
-    return res.status(401).json({ message: 'Invalid credentials' });
+// Middleware function to verify JWT token
+function verifyToken(req, res, next) {
+  // Get token from authorization header
+  const token = req.headers['authorization'];
+  if (!token) {
+    return res.status(401).json({ error: 'Unauthorized access' });
   }
-
-  // Successful login
-  res.json({ message: 'Login successful' });
-});
-
-// Start server
-app.listen(3000, () => console.log('Server started on port 3000'));
-module.exports = { authenticate };
+  try {
+    // Verify token using the secret key
+    const decoded = jwt.verify(token, secretKey);
+    // Add the user information to the request object
+    req.user = decoded;
+    next(); 
+  } catch (err) {
+    return res.status(401).json({ error: 'Unauthorized access' });
+  }
+}
+// Function to generate JWT token
+function generateToken(user) {
+  const payload = {
+    id: user._id,
+    email: user.email,
+    role: user.role // Admin or Employee
+  };
+  return jwt.sign(payload, secretKey, { expiresIn: '1h' });
+}
+module.exports = { verifyToken, generateToken };
