ca358027678ef1db37198abc96cf3135c126ff71
Isaac Orsaah <66434358+isaacorsaah@users.noreply.github.com>
2023-06-30 13:44:09 +0100

    auth service

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,25 +1,21 @@
-// auth.service.ts
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
 export class AuthService {
-  headers = new HttpHeaders({'Content-Type' : 'application/json'});
-  private postHeadedr = {headers: this.headers, withCredentials: true };
-  private getHeadedr = {withCredentials: true};
+  private apiUrl = 'http://localhost:3000/api/login';
+
   constructor(private http: HttpClient) { }
 
-  public register(data: any): Observable<any> {
-    return this.http.post('http://localhost:3000/api/register', data, this.postHeadedr);
+  login(username: string, password: string) {
+    const body = {
+      username: username,
+      password: password
+    };
+
+    return this.http.post(this.apiUrl, body);
   }
 
-  public login(username: string, password: string): Observable<any> {
-    return this.http.post('http://localhost:3000/api/login', { username, password }, this.postHeadedr);
-  }
-  public test():Observable<any>{
-    return this.http.get('http://localhost:3000/api/test', this.getHeadedr);
-  }
 }
--- src/app/db.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/hr-management-system', { useNewUrlParser: true });
-mongoose.connection.on('error', console.error.bind(console, 'MongoDB connection error:'));
-mongoose.connection.once('open', () => {
-  console.log('MongoDB connected successfully!');
-});
