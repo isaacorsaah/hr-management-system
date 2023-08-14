a6e68996c7fb1a1fe7825e29c966a4ac1136babf
Isaac Orsaah <66434358+isaacorsaah@users.noreply.github.com>
2023-08-14 07:04:51 +0100

    time and leave management for employes

--- src/app/employee-dashboard/employee-dashboard.component.ts
+++ src/app/employee-dashboard/employee-dashboard.component.ts
@@ -1,15 +1,47 @@
 import { Component, OnInit } from '@angular/core';
+import { EmployeeService } from '../employee.service';
 
 @Component({
-  selector: 'app-employee-dashboard',
-  templateUrl: './employee-dashboard.component.html',
-  styleUrls: ['./employee-dashboard.component.css']
+    selector: 'app-employee-dashboard',
+    templateUrl: './employee-dashboard.component.html',
+    styleUrls: ['./employee-dashboard.component.css']
 })
 export class EmployeeDashboardComponent implements OnInit {
 
-  constructor() { }
+    employeeName: string = '';
+    timeOn: string = '8 hours';
+    timeOff: string = '2 hours';
+    
+    logDate!: string;
+    startTime!: string;
+    endTime!: string;
+    leaveStart!: string;
+    leaveEnd!: string;
+    leaveReason!: string;
+    currentView: string = 'dashboard';
 
-  ngOnInit(): void {
-  }
+    constructor(private employeeService: EmployeeService) { }
 
+    ngOnInit(): void {
+      this.employeeService.getLoggedInEmployee().subscribe(
+        (data) => {
+          this.employeeName = data.name;
+        },
+        (error) => {
+          console.error('Error fetching logged-in employee:', error);
+        }
+      );
+    }
+
+    navigate(view: string): void {
+        this.currentView = view;
+    }
+
+    logTime(): void {
+        console.log('Time logged for:', this.logDate, this.startTime, 'to', this.endTime);
+    }
+
+    applyForLeave(): void {
+        console.log('Leave applied from', this.leaveStart, 'to', this.leaveEnd, 'for reason:', this.leaveReason);
+    }
 }
--- src/app/employee.service.ts
+++ src/app/employee.service.ts
@@ -11,12 +11,19 @@ export class EmployeeService {
   constructor(private http: HttpClient) { }
 
   getEmployees() { return this.http.get(`${this.apiUrl}/getEmployees`); }
-
-  addEmployee(employee: any, password: string): Observable<any> { 
+  addEmployee(employee: any, password: string): Observable<any> {
     const fullEmployeeData = {...employee, password};
-    return this.http.post(`${this.apiUrl}/addEmployee`, fullEmployeeData); }
-
+    return this.http.post(`${this.apiUrl}/addEmployee`, fullEmployeeData);
+  }
   editEmployee(id: string, employeeData: any): Observable<any> { console.log(id); return this.http.put(`${this.apiUrl}/editEmployee/${id}`, employeeData); }
-
   deleteEmployee(id: string): Observable<any> { return this.http.delete(`${this.apiUrl}/deleteEmployee/${id}`); }
+  
+
+  getLoggedInEmployee(): Observable<any> { return this.http.get(`${this.apiUrl}/loggedInEmployee`); }
+
+  getEmployeeInfo(): Observable<any> { return this.http.get(`${this.apiUrl}/employeeInfo`); }
+  getLogHours(): Observable<any> { return this.http.get(`${this.apiUrl}/logHours`); }
+  getTimeOff(): Observable<any> { return this.http.get(`${this.apiUrl}/timeOff`); }
+  getScheduledMeetings(): Observable<any> { return this.http.get(`${this.apiUrl}/scheduledMeetings`); }
+  getTasks(): Observable<any> { return this.http.get(`${this.apiUrl}/tasks`); }
 }
